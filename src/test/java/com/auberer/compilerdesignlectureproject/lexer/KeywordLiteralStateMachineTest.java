96ee2a3990ec0ffd34cba891a5088ac4b7f7bdf3
Marc Auberer <marc.auberer@chillibits.com>
2025-04-27 12:30:30 +0200

    Fix tests

--- src/test/java/com/auberer/compilerdesignlectureproject/lexer/KeywordLiteralStateMachineTest.java
+++ src/test/java/com/auberer/compilerdesignlectureproject/lexer/KeywordLiteralStateMachineTest.java
@@ -1,5 +1,7 @@
 package com.auberer.compilerdesignlectureproject.lexer;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,68 +10,78 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class KeywordLiteralStateMachineTest {
 
-    @Test
-    @DisplayName("Happy path: Valid keywords")
-    public void testValidKeywords() {
-        String[] validKeywords = {
-                "if",
-                "else",
-                "while",
-                "for",
-                "return",
-                "int",
-                "float",
-                "char",
-                "boolean",
-                "switch"
-        };
+  @Data
+  @AllArgsConstructor
+  static class KeywordTokenTypePair {
+    private String keyword;
+    private TokenType tokenType;
+  }
 
-        for (String keyword : validKeywords) {
-            KeywordStateMachine stateMachine = new KeywordStateMachine(keyword, KeywordStateMachine.getTokenType(keyword));
-            stateMachine.init();
-            stateMachine.reset();
+  @Test
+  @DisplayName("Happy path: Valid keywords")
+  public void testValidKeywords() {
+    KeywordTokenTypePair[] validKeywordTokenPairs = {
+        new KeywordTokenTypePair("if", TokenType.TOK_IF),
+        new KeywordTokenTypePair("else", TokenType.TOK_ELSE),
+        new KeywordTokenTypePair("while", TokenType.TOK_WHILE),
+        new KeywordTokenTypePair("for", TokenType.TOK_FOR),
+        new KeywordTokenTypePair("switch", TokenType.TOK_SWITCH),
+        new KeywordTokenTypePair("ret", TokenType.TOK_RET),
+        new KeywordTokenTypePair("int", TokenType.TOK_TYPE_INT),
+        new KeywordTokenTypePair("double", TokenType.TOK_TYPE_DOUBLE),
+        new KeywordTokenTypePair("string", TokenType.TOK_TYPE_STRING),
+        new KeywordTokenTypePair("bool", TokenType.TOK_TYPE_BOOL),
+    };
 
-            for (char c : keyword.toCharArray()) {
-                assertDoesNotThrow(() -> stateMachine.processInput(c), "Exception for keyword: " + keyword);
-            }
+    for (KeywordTokenTypePair keywordTokenTypePair : validKeywordTokenPairs) {
+      String keyword = keywordTokenTypePair.getKeyword();
+      TokenType tokenType = keywordTokenTypePair.getTokenType();
 
-            assertTrue(stateMachine.isInAcceptState(), "Should be in accept state for keyword: " + keyword);
-        }
+      KeywordStateMachine stateMachine = new KeywordStateMachine(keyword, tokenType);
+      stateMachine.init();
+      stateMachine.reset();
+
+      for (char c : keyword.toCharArray()) {
+        assertDoesNotThrow(() -> stateMachine.processInput(c), "Exception for keyword: " + keywordTokenTypePair);
+      }
+
+      assertTrue(stateMachine.isInAcceptState(), "Should be in accept state for keyword: " + keywordTokenTypePair);
     }
+  }
 
-    @Test
-    @DisplayName("Erroneous input: Invalid keywords")
-    public void testInvalidKeywords() {
-        String[] invalidKeywords = {
-                "",           // empty
-                "iff",        // typo
-                "els",        // incomplete keyword
-                "whileloop",  // valid keyword but extra characters
-                "forEach",    // case-sensitive mismatch
-                "return1",    // valid keyword but invalid suffix
-                "int32",      // valid keyword but invalid suffix
-                "Float",      // case-sensitive mismatch
-                "booleanValue", // valid keyword but invalid suffix
-                "switcheroo"  // valid keyword but invalid suffix
-        };
+  @Test
+  @DisplayName("Erroneous input: Invalid keywords")
+  public void testInvalidKeywords() {
+    String[] invalidKeywords = {
+        "",           // empty
+        "iff",        // typo
+        "els",        // incomplete keyword
+        "whileloop",  // valid keyword but extra characters
+        "forEach",    // case-sensitive mismatch
+        "return1",    // valid keyword but invalid suffix
+        "int32",      // valid keyword but invalid suffix
+        "Float",      // case-sensitive mismatch
+        "booleanValue", // valid keyword but invalid suffix
+        "switcheroo"  // valid keyword but invalid suffix
+    };
 
-        for (String keyword : invalidKeywords) {
-            KeywordStateMachine stateMachine = new KeywordStateMachine("if", KeywordStateMachine.getTokenType("if"));
-            stateMachine.init();
-            stateMachine.reset();
+    for (String keyword : invalidKeywords) {
+      KeywordStateMachine stateMachine = new KeywordStateMachine("if", TokenType.TOK_IF);
+      stateMachine.init();
+      stateMachine.reset();
 
-            boolean threwException = false;
+      boolean threwException = false;
 
-            try {
-                for (char c : keyword.toCharArray()) {
-                    stateMachine.processInput(c);
-                }
-            } catch (IllegalStateException e) {
-                threwException = true;
-            }
-
-            assertTrue(threwException || !stateMachine.isInAcceptState(),
-                    "Should not be in accept state for keyword: " + keyword);
+      try {
+        for (char c : keyword.toCharArray()) {
+          stateMachine.processInput(c);
         }
+      } catch (IllegalStateException e) {
+        threwException = true;
+      }
+
+      assertTrue(threwException || !stateMachine.isInAcceptState(),
+          "Should not be in accept state for keyword: " + keyword);
     }
+  }
 }
