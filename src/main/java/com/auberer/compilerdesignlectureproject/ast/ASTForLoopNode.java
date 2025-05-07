47b47a358da5025ce1a0d1e8ee29d16b7680aa91
Larry <larry.knopf@web.de>
2025-05-07 12:00:40 +0200

    AST Helpers - Team 5 (#44)

--- src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTForLoopNode.java
+++ src/main/java/com/auberer/compilerdesignlectureproject/ast/ASTForLoopNode.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 public class ASTForLoopNode extends ASTNode {
 
+
   @Override
   public <T> T accept(ASTVisitor<T> visitor) {
     return visitor.visitForLoop(this);
@@ -14,5 +15,22 @@ public class ASTForLoopNode extends ASTNode {
   public static Set<TokenType> getSelectionSet() {
     return Set.of(TokenType.TOK_FOR);
   }
+
+  public ASTVarDeclNode getInitialization() {
+    return getChild(ASTVarDeclNode.class, 0);
+  }
+
+  public ASTTernaryExprNode getCondition() {
+    return getChild(ASTTernaryExprNode.class, 0);
+  }
+
+  public ASTAssignExprNode getIncrement() {
+    return getChild(ASTAssignExprNode.class, 0);
+  }
+
+  public ASTStmtLstNode getBody() {
+    return getChild(ASTStmtLstNode.class, 0);
+  }
+
 
 }
